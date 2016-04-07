4ad4a92114c5dc09264575d027081e4c6737759a
Erik Garrison <erik.garrison@gmail.com>
2016-04-08 01:55:27 +0200

    updates hhga script

--- scripts/hhga_region.sh
+++ scripts/hhga_region.sh
@@ -1,27 +1,30 @@
 #!/bin/bash
 
-if [ $# -ne 2 ];
+if [ $# -ne 4 ];
 then
-echo usage: $0 '[region] [window_size]'
+echo usage: $0 '[window_size] [region] [vcf_dir] [output_dir]'
 echo generates hhga data for this region in the current directory
 exit
 fi
 
 region=$1
 window_size=$2
+vcf_dir=$3
+output_dir=$4
 ref=~/graphs/hs37d5.fa
 dir=~/graphs/NA12878
 input_bam=$dir/XPrize_Illumina_WG.bam
-variant_calls=$dir/XPrize_Illumina_WG.vcf.gz
+variant_calls=$vcf_dir/$region.vcf.gz
 giab_truth=$dir/NISTIntegratedCalls_14datasets_131103_allcall_UGHapMerge_HetHomVarPASS_VQSRv2.19_2mindatasets_5minYesNoRatio_all_nouncert_excludesimplerep_excludesegdups_excludedecoy_excludeRepSeqSTRs_noCNVs.vcf.gz
 giab_callable=$dir/union13callableMQonlymerged_addcert_nouncert_excludesimplerep_excludesegdups_excludedecoy_excludeRepSeqSTRs_noCNVs_v2.19_2mindatasets_5minYesNoRatio.bed.gz
 
-giab_region=$region.giab_truth.vcf.gz
-normalized_variants=$region.norm.vcf.gz
-callable_variants=$region.giab_callable.vcf.gz
-uncallable_variants=$region.giab_uncallable.vcf.gz
-passing_variants=$region.giab_pass.vcf.gz
-failing_variants=$region.giab_fail.vcf.gz
+giab_region=$output_dir/$region.giab_truth.vcf.gz
+normalized_variants=$output_dir/$region.norm.vcf.gz
+callable_variants=$output_dir/$region.giab_callable.vcf.gz
+uncallable_variants=$output_dir/$region.giab_uncallable.vcf.gz
+passing_variants=$output_dir/$region.giab_pass.vcf.gz
+failing_variants=$output_dir/$region.giab_fail.vcf.gz
+hhga_truthiness=$output_dir/$region.truthiness.hhga.gz
 
 echo getting giab region
 tabix -h $giab_truth $region | bgziptabix $giab_region
@@ -37,17 +40,18 @@ echo intersecting with giab
 vcfintersect -r $ref -i $giab_region $callable_variants | bgziptabix $passing_variants
 vcfintersect -v -r $ref -i $giab_region $callable_variants | bgziptabix $failing_variants
 
-hhga_callability=$region.callable.hhga.bz2
-hhga_truthiness=$region.truthiness.hhga.bz2
-
-echo generating callability hhga
-
-( hhga -f $ref -r $region -b $input_bam -v $callable_variants -w $window_size -c 1
-  hhga -f $ref -r $region -b $input_bam -v $uncallable_variants -w $window_size -c -1
-) | shuf | bzip2 >$hhga_callability
-
 echo generating truthiness hhga
 
 ( hhga -f $ref -r $region -b $input_bam -v $passing_variants -w $window_size -c 1
   hhga -f $ref -r $region -b $input_bam -v $failing_variants -w $window_size -c -1
-) | shuf | bzip2 >$hhga_truthiness
+) | shuf | gzip >$hhga_truthiness
+
+echo cleaning up
+
+rm -f $giab_region*
+rm -f $normalized_variants*
+rm -f $callable_variants*
+rm -f $uncallable_variants*
+rm -f $passing_variants*
+rm -f $failing_variants*
+
